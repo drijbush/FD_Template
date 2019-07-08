e9db935b3c0fe9d85a11f94b1836f69dbf487b28
me <my.business@not.yours>
2019-07-08 12:59:18 +0100

    First commit of more or less empty code
    Numbers_module
    
    Very start of vecs_module

---
+++ FD_template.f90
@@ -0,0 +1,21 @@
+
+Module numbers_module
+
+  Implicit None
+
+  Private
+
+  Integer, Parameter, Public :: wp = Selected_real_kind( 12, 70 )
+  
+End Module numbers_module
+
+Module vecs
+
+  Use numbers_module, Only : wp
+
+  Implicit None
+
+  Private
+
+  
+End Module vecs
