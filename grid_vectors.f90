667a897d7c4651b2a441467b12ab16fda677bd12
Jacob Wilkins <jacob.wilkins@oerc.ox.ac.uk>
2019-07-10 14:54:31 +0100

    Added some ford docs, fixed bug with cmake, added openMP to cmake

--- grid_vectors.f90
+++ grid_vectors.f90
@@ -5,16 +5,16 @@ Module grid_vectors_module
   Implicit None
 
   Type, Public :: grid_vectors
-     Real( wp ), Dimension( :, : ), Allocatable, Private :: dir_vecs
-     Real( wp ),                                 Private :: volume
-     Real( wp ), Dimension( :, : ), Allocatable, Private :: inv_vecs
-   Contains
-     Procedure, Public :: set_dir_vecs
-     Procedure, Public :: get_dir_vec
-     Procedure, Public :: get_volume
-     Procedure, Public :: get_inv_vec
+    Real( wp ), Dimension( :, : ), Allocatable, Private :: dir_vecs
+    Real( wp ),                                 Private :: volume
+    Real( wp ), Dimension( :, : ), Allocatable, Private :: inv_vecs
+  Contains
+    Procedure, Public :: set_dir_vecs
+    Procedure, Public :: get_dir_vec
+    Procedure, Public :: get_volume
+    Procedure, Public :: get_inv_vec
   End type grid_vectors
-  
+
   Private
 
 Contains
@@ -25,10 +25,10 @@ Contains
     Real( wp ), Dimension( :, : ), Intent( In    ) :: vecs
 
     Real( wp ) :: V
-    
+
     g%dir_vecs = vecs
     Allocate( g%inv_vecs, Mold = g%dir_vecs )
-    
+
     Call givens_invert( g%dir_vecs, g%inv_vecs, V )
     g%volume = Abs( V )
 
@@ -37,29 +37,29 @@ Contains
   Pure Function get_dir_vec( g, which ) Result( v )
 
     Real( wp ), Dimension( : ), Allocatable :: v
-    
+
     Class( grid_vectors ), Intent( In ) :: g
     Integer              , Intent( In ) :: which
 
     v = g%dir_vecs( :, which )
-    
+
   End Function get_dir_vec
 
   Pure Function get_inv_vec( g, which ) Result( v )
 
     Real( wp ), Dimension( : ), Allocatable :: v
-    
+
     Class( grid_vectors ), Intent( In ) :: g
     Integer              , Intent( In ) :: which
 
     v = g%inv_vecs( which, : )
-    
+
   End Function get_inv_vec
 
   Pure Function get_volume( g ) Result( V )
 
     Real( wp ) :: V
-    
+
     Class( grid_vectors ), Intent( In ) :: g
 
     V = g%volume
@@ -111,50 +111,50 @@ Contains
     L = A
     Q = 0.0_wp
     Do i = 1, dim
-       Q( i, i ) = 1.0_wp
+      Q( i, i ) = 1.0_wp
     End Do
     LQ_factorise: Do m = 2, dim
-       Do n = 1, m - 1
-          ! For element mn generate the rotation which
-          ! will zweo that element
-          ann = L( n, n )
-          anm = L( n, m )
-          theta = atan2( - anm, ann )
-          c = Cos( theta )
-          s = Sin( theta )
-          ! And apply that rotation to the original matrix
-          Do i = 1, Dim
-             Lim = L( i, m )
-             Lin = L( i, n )
-             L( i, n ) = c * Lin - s * Lim
-             L( i, m ) = c * Lim + s * Lin
-          End Do
-          ! And then apply the rotation to the orthognal matrix
-          Do i = 1, Dim
-             Qmi = Q( m, i )
-             Qni = Q( n, i )
-             Q( n, i ) = c * Qni - s * Qmi 
-             Q( m, i ) = c * Qmi + s * Qni
-          End Do
-       End Do
+      Do n = 1, m - 1
+        ! For element mn generate the rotation which
+        ! will zweo that element
+        ann = L( n, n )
+        anm = L( n, m )
+        theta = atan2( - anm, ann )
+        c = Cos( theta )
+        s = Sin( theta )
+        ! And apply that rotation to the original matrix
+        Do i = 1, Dim
+          Lim = L( i, m )
+          Lin = L( i, n )
+          L( i, n ) = c * Lin - s * Lim
+          L( i, m ) = c * Lim + s * Lin
+        End Do
+        ! And then apply the rotation to the orthognal matrix
+        Do i = 1, Dim
+          Qmi = Q( m, i )
+          Qni = Q( n, i )
+          Q( n, i ) = c * Qni - s * Qmi
+          Q( m, i ) = c * Qmi + s * Qni
+        End Do
+      End Do
     End Do LQ_factorise
 
     ! Now invert the triangular matrix, returning the inverse in B
     B(  1, 1  ) = 1.0_wp / L( 1, 1 )
     B(  1, 2: ) = 0.0_wp
     Do m = 2, dim
-       Do n = 1, m
-          mv = 0.0_wp
-          Do i = 1, m - 1
-             mv = mv - L( m, i ) * B( i, n )
-          End Do
-          If( m == n ) Then
-             mv = mv + 1.0_wp
-          End If
-          mv = mv / L( m, m )
-          B( m, n ) = mv
-       End Do
-       B( m, n: ) = 0.0_wp
+      Do n = 1, m
+        mv = 0.0_wp
+        Do i = 1, m - 1
+          mv = mv - L( m, i ) * B( i, n )
+        End Do
+        If( m == n ) Then
+          mv = mv + 1.0_wp
+        End If
+        mv = mv / L( m, m )
+        B( m, n ) = mv
+      End Do
+      B( m, n: ) = 0.0_wp
     End Do
 
     ! The inverse of A is now trivially formed
@@ -162,15 +162,15 @@ Contains
 
     ! And generate the determinant
     If( Present( det ) ) Then
-       det = 1.0_wp
-       Do i = 1, dim
-          det = det * L( i, i )
-       End Do
+      det = 1.0_wp
+      Do i = 1, dim
+        det = det * L( i, i )
+      End Do
     End If
 
     Deallocate( Q )
     Deallocate( L )
 
   End Subroutine givens_invert
-  
+
 End Module grid_vectors_module
